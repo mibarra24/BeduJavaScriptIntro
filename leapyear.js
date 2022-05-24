a38387f5baf1608eb4ec15fef7325674b51e882c
mibarra24 <marcodzeko@gmail.com>
2022-05-24 14:29:17 -0700

    Exercice 2 and 3 completed

---
+++ leapyear.js
@@ -0,0 +1,13 @@
+//program to check leap year
+function checkLeapYear(year){
+    const leap = new Date(year,1,29).getDate() ===29;
+    if(leap){
+        console.log(year+'is a leap year');
+    } else{
+        console.log(year+'is not a leap year');
+    }
+}
+
+//take input
+const year = prompt('Enter a year')
+checkLeapYear(year);
---
+++ microlearning1.js
@@ -0,0 +1,30 @@
+// Ejercicio 1 y 2
+var nombre = "Marco"; //Cadena de texto
+var edad = 29; //numero
+var pi = Math.PI; //numero
+var altura = 180; //numero
+var escuela = "Licenciado Luis Encinas"; //Cadena de texto
+
+//Ejercicio 3
+//En una escuela hay 5 grupos cada uno tiene 20 alumnos, para el festival de primavera se requieren
+//15 flores de papel china por cada alumno al dia, y el festival tendra una duracion de 2 dias
+//Cuantas flores se necesitan?
+
+var numgrupos = 5;
+var numalumnos = 20;
+
+totalalumnos = (numgrupos * numalumnos);
+
+console.log(totalalumnos)
+
+var floresporalumno = 15
+var dias = 2
+
+totalfloresporalumno = ((floresporalumno * dias) * totalalumnos)
+
+console.log(totalfloresporalumno)
+
+
+//
+
+
